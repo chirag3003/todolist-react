a491ba6e4be29d4e7f0744a01b24bdffc065145d
chirag <chiragbhalotia0412@gmail.com>
2021-05-10 16:56:08 +0530

    added buld folder to host on github

---
+++ build/precache-manifest.ad152cc462bc1fdfc5ec5dac4b759817.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7ec1ca7ae330f1c1b51154ce1674f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97d2342d8bd0ff1f5482",
+    "url": "/static/js/2.dc837827.chunk.js"
+  },
+  {
+    "revision": "2fc173b2471844c67c52",
+    "url": "/static/js/main.d438af9a.chunk.js"
+  },
+  {
+    "revision": "164da21e09e43c8bb0c6",
+    "url": "/static/js/runtime-main.aa0ae0cd.js"
+  }
+]);
